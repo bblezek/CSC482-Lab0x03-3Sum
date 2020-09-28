1be52d27b27c8bfc49204a25f835d3c2cc95cce5
bblezek <Brin.blezek@trojans.dsu.edu>
2020-09-28 11:06:28 -0500

    - Modified myThreeSum to be clearer and faster - Also added testing function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,68 +1,185 @@
 package com.company;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
 import java.util.Arrays;
+import java.util.Random;
 
 public class Main {
 
-    public static boolean myThreeSum(int[] array, int arrayLength){
+    public static int[] generateArray(int length, int min, int max) {
+        Random rand = new Random();
+        int[] array = new int[length];
+        for (int x = 0; x < length; x++) {
+            array[x] = rand.nextInt(max - min) + min;
+        }
+        return array;
+    }
+
+    public static boolean myThreeSum(int[] array, int arrayLength) {
         Arrays.sort(array);
         int midIndex = 0;
-        int posLow, negLow, highChecked, lowChecked;
-        for(int x = 0; x < arrayLength; x++){
-            if(array[x] > 0){
+        int posLow, posHigh, negLow, negHigh, highChecked, lowChecked;
+        for (int x = 0; x < arrayLength; x++) {
+            if (array[x] > 0) {
                 midIndex = x;
                 break;
             }
         }
 
-        for(int neg = 0; neg < midIndex; neg++){
+        highChecked = arrayLength - 1;
+        //Looking for a one negative number and two positive solution
+        //Taking lowest negative first
+        for (int neg = 0; neg < midIndex; neg++) {
+            //Resetting position low value
             posLow = midIndex;
-            for(int posHigh = arrayLength-1; posHigh > posLow; posHigh--){
-                    while (posLow < posHigh && (array[neg] + array[posHigh] + array[posLow] < 0)) {
-                        posLow++;
-                    }
-                    if ((array[neg] + array[posHigh] + array[posLow] == 0) && array[posHigh] != array[posHigh]) {
-                        System.out.printf("Match found at indices %d, %d and %d\n", posHigh, posLow, neg);
-                        return true;
-                    }
+            posHigh = highChecked;
+            //Looping down from highest element to find highest element smaller than current negative
+            while(array[posHigh] + array[neg] > 0){
+                posHigh--;
+            }
+
+            //Setting highChecked to highest positive element that is smaller than current negative for next round
+            //of negative element
+            highChecked = posHigh;
+
+            //Finding matching elements by looping down and up through positive elements
+            while(posHigh > posLow) {
+                //If total equals 0, return
+                if(array[posHigh] + array[posLow] + array[neg] == 0 && array[posHigh] != array[posLow]){
+                    return true;
+                //If total is greater than 0, decrement high positive index
+                } else if(array[posHigh] + array[posLow] + array[neg] > 0){
+                    posHigh--;
+                //If total is less than 0, increment low positive index
+                } else {
+                    posLow++;
+                }
             }
         }
 
+        lowChecked = 0;
 
-        for(int pos = midIndex; pos < arrayLength; pos++){
-            negLow = midIndex-1;
-            for(int negHigh = 0; negHigh < negLow; negHigh++){
-                while(negLow > negHigh && (array[pos] + array[negHigh] + array[negLow]) > 0){
-                    negLow--;
-                }
-                if ((array[pos] + array[negHigh] + array[negLow] == 0) && array[negHigh] != array[negLow]) {
-                    System.out.printf("Match found at indices %d, %d and %d\n", negHigh, negLow, pos);
+        //Doing same thing for one positive and two negative number solution
+        for (int pos = midIndex; pos < arrayLength; pos++) {
+            negLow = midIndex - 1;
+            negHigh = lowChecked;
+            //Looping "up" through negative elements to find largest negative element larger than current negative
+            while(array[negHigh] + array[pos] < 0){
+                negHigh++;
+            }
+
+            //Setting lowChecked to largest negative element that is smaller than the current positive for next round
+            //of positive elements
+            lowChecked = negHigh;
+
+            //Finding matching elements by looping down and up through positive elements
+            while(negHigh < negLow){
+                //If total equals 0, return
+                if(array[negHigh] + array[negLow] + array[pos] == 0 && array[negHigh] != array[negLow]){
                     return true;
+                } else if(array[negHigh] + array[negLow] + array[pos] < 0) {
+                    negHigh++;
+                } else{
+                    negLow--;
                 }
             }
         }
         return false;
     }
 
-    public static boolean bruteThreeSum(int[] array, int arrayLength){
-        for(int i=0; i<arrayLength-2; i++){
-            for(int j = i+1; j<arrayLength-1; j++){
-                for(int k = j+1; k<arrayLength; k++){
-                    if((array[i]+array[j]+array[k]) == 0 && array[i] != array[j] && array[j] != array[k] && array[i] != array[k]){
-                        System.out.printf("Match found at indices %d, %d and %d\n", i, j, k);
+    public static boolean bruteThreeSum(int[] array, int arrayLength) {
+        for (int i = 0; i < arrayLength - 2; i++) {
+            for (int j = i + 1; j < arrayLength - 1; j++) {
+                for (int k = j + 1; k < arrayLength; k++) {
+                    if ((array[i] + array[j] + array[k]) == 0 && array[i] != array[j] && array[j] != array[k] && array[i] != array[k]) {
+                      //  System.out.printf("Match found at indices %d, %d and %d\n", i, j, k);
                         return true;
                     }
                 }
             }
         }
         return false;
+    }
+
+    //Function to retrieve CPU time
+    public static long getCpuTime() {
+        ThreadMXBean bean = ManagementFactory.getThreadMXBean();
+        return bean.isCurrentThreadCpuTimeSupported() ?
+                bean.getCurrentThreadCpuTime() : 0L;
+    }
+
+    public static boolean verificationTests() {
+        int[] testArray;
+        boolean bruteResult, myResult;
+        for(int N = 4; N < 10000; N=N*2 ){
+            System.out.printf("Testing for array of length %d\n", N);
+            testArray = generateArray(N, -(N*2), N*2);
+                bruteResult = bruteThreeSum(testArray, N);
+                myResult = myThreeSum(testArray, N);
+                if (bruteResult != myResult){
+                    System.out.printf("Three sum results don't match for array of size %d\n ", N);
+                    return false;
+                }
+                System.out.printf("Results match - tests passed!\n");
+            if(N < 100){
+                for(int x = 0; x < N; x++){
+                    System.out.printf("%d ", testArray[x]);
+                }
+                System.out.printf("\n");
+            }
+        }
+        return true;
+    }
+
+
+    public static void runTimeTests() {
+        int[] testArray = new int[1];
+        long beforeTime, afterTime, totalTime;
+        long brutePrevTime = 1;
+
+        System.out.printf("%31s %47s %47s\n", "Brute 3sum", "Faster 3sum", "Fastest 3sum");
+        System.out.printf("%15s %15s %15s %15s %15s %15s %15s %15s %15s %15s\n",
+                "N", "Time", "Doubling", "Expected", "Time", "Doubling", "Expected", "Time", "Doubling", "Expected");
+        System.out.printf("%47s %15s %31s %15s %31s %15s\n", "Ratios", "Doubling", "Ratios", "Doubling", "Ratios", "Doubling");
+        System.out.printf("%63s %47s %47s\n", "Ratios", "Ratios", "Ratios");
+        for (int N = 4; N < 1000; N = N * 2) {
+            System.out.printf("%16s", N);
+            testArray = generateArray(N, -(N * 2), N * 2);
+
+            beforeTime = getCpuTime();
+            bruteThreeSum(testArray, N);
+            afterTime = getCpuTime();
+            totalTime = afterTime - beforeTime;
+            System.out.printf("%15d", totalTime);
+            if(N!= 4){
+                System.out.printf("%15.3f %15d", (float) totalTime/brutePrevTime, (int)(Math.pow(N, 3)/(Math.pow((N/2), 3))));
+            } else {
+                System.out.printf("%15s %15s", "na", "na");
+            }
+            brutePrevTime = totalTime;
+
+            beforeTime = getCpuTime();
+            bruteThreeSum(testArray, N);
+            afterTime = getCpuTime();
+            totalTime = afterTime - beforeTime;
+            System.out.printf("%15d", totalTime);
+            if(N!= 4){
+                System.out.printf("%15.3f %15d", (float) totalTime/brutePrevTime, (int)(Math.pow(N, 3)/(Math.pow((N/2), 3))));
+            } else {
+                System.out.printf("%15s %15s", "na", "na");
+            }
+            brutePrevTime = totalTime;
+
+            System.out.printf("\n");
+        }
     }
 
     public static void main(String[] args) {
-	// write your code here
-        int[] array = {-19, -14, -7, -3, 4, 6, 1, 19};
-        if(bruteThreeSum(array, 8)){
-            System.out.printf("Match found!\n");
+        // write your code here
+        if(verificationTests()){
+            System.out.printf("Tests passed!");
         }
+        runTimeTests();
     }
 }
